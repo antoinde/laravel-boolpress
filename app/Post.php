f737bf414c32bbc97de4be3ba66898a0f7bc8b73
antoinde <antonello.indelicato98@gmail.com>
2022-11-16 00:04:51 +0100

    add fillable into posts

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+
+    // titolo
+    // contenuto
+    // slug (serve per identificare la risorsa univocamente tramite url)
+    protected $fillable=['title', 'content', 'slug'];
 }
