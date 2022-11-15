e5cd014ed6f96e6a373b2e39fb146e67f24fb3ce
antoinde <antonello.indelicato98@gmail.com>
2022-11-15 21:21:07 +0100

    add guest div into home view

---
+++ resources/views/guests/home.blade.php
@@ -0,0 +1,21 @@
+
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                
+               <div>Guest page</div>
+               <div>Guest page</div>
+               <div>Guest page</div>
+               <div>Guest page</div>
+               <div>Guest page</div>
+               <div>Guest page</div>
+               <div>Guest page</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
