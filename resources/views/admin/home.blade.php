9a914fe1089d774e627ff4b829d04a4b65f8e261
antoinde <antonello.indelicato98@gmail.com>
2022-11-15 21:14:56 +0100

    add admin area div into home view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,7 +6,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                
                 <div class="card-header">{{ __('Dashboard') }}</div>
+                
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,6 +18,14 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
+                    <div>Admin area</div>
                 </div>
             </div>
         </div>
