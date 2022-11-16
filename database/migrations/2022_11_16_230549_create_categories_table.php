cd09ab616fdf672dc35b57a21069d1f9a57e385a
antoinde <antonello.indelicato98@gmail.com>
2022-11-17 00:12:03 +0100

    set table migration category

--- database/migrations/2022_11_16_230549_create_categories_table.php
+++ database/migrations/2022_11_16_230549_create_categories_table.php
@@ -15,6 +15,10 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 30);
+            $table->string('slug')->unique();
+            
             $table->timestamps();
         });
     }
