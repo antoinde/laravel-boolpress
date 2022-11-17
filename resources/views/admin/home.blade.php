ed5bccda049c564e46b5fc2fdab101db3a8b4fb4
antoinde <antonello.indelicato98@gmail.com>
2022-11-17 03:42:54 +0100

    fixed dashboard home view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,6 +3,22 @@
 
 @section('content')
 <div class="container">
-   admin
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-header">{{ __('Dashboard') }}</div>
+
+               <div class="card-body">
+                   @if (session('status'))
+                       <div class="alert alert-success" role="alert">
+                           {{ session('status') }}
+                       </div>
+                   @endif
+
+                   {{ __('You are logged in as an administrator!') }}
+               </div>
+           </div>
+       </div>
+   </div>
 </div>
 @endsection
