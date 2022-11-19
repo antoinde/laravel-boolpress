9f7e47b4b8fceecf8de9a46eb1ccaa7fabb57e3c
antoinde <antonello.indelicato98@gmail.com>
2022-11-19 19:47:19 +0100

    add link "back to posts" in create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <h1>CREATE NEW POST</h1>
 
+<div class="row">
+    <div class="col-12">
+        <a class="btn btn-primary" href="{{ route('admin.posts.index') }}">Back to posts</a>
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <hr>
+    </div>
+</div>
+
 <form action="{{ route('admin.posts.store') }}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('POST')
@@ -17,7 +28,7 @@
     </div>
 
     <div>
-        <label for="category_id">Categoria:</label>
+        <label for="category_id">Category</label>
         <select name="category_id">
             <option value="">--Seleziona--</option>
             @foreach ($categories as $category)
@@ -36,14 +47,7 @@
         @enderror
     </div>
 
-    <div class="form-group">
-        <label for="image">Image</label>
-        <input type="file" name="image" id="image" class="form-control-file">
-        @error('image')
-            <div class="alert alert-danger">{{ $message }}</div>
-        @enderror
-    </div>
-
     <input type="submit" value="Save" class="btn btn-primary">
 </form>
+
 @endsection
