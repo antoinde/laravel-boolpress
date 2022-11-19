48b4a97c896c275155aeab2439d8d91e0ee17230
antoinde <antonello.indelicato98@gmail.com>
2022-11-19 19:14:54 +0100

    Sistemata view show

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<h1>TITOLO: {{ $post->title }}</h1>
+<h1>TITOLO:</h1>
+<h4> {{ $post->title }}</h4>
 
-<p>CONTENUTO: {{ $post->content }}</p>
+<h2>CONTENUTO:</h2>
+<p> {{ $post->content }}</p>
 
 @if ($post->category)
 
-    <p>CATEGORIA: {{ $post->category->name }}</p>
+    <h3>CATEGORIA:</h3>
+    <p> {{ $post->category->name }}</p>
 @else
-    <p>CATEGORIA: Nessuna categoria</p>
+    <h3>CATEGORIA:</h3>
+    <p>Nessuna categoria</p>
 @endif
 
 
