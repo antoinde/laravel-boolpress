7fa85b21ea4860fc3494c61d5fb8550e7f3d968a
antoinde <antonello.indelicato98@gmail.com>
2022-11-20 04:38:12 +0100

    Add deleting categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -7,7 +7,14 @@
                 <h1>ALL CATEGORIES</h1>
                 <ul>
                     @foreach ($categories as $category)
-                        <li><h4><strong>{{ $category->name }}</strong></h4></li>
+                        <li>
+                            <h5><strong>{{ $category->name }}</strong></h5> 
+                            <form action="{{ route('admin.categories.destroy', $category->id) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button onclick="confirm('Are you sure? You really want to delete it?')" class="btn btn-danger" type="submit">Elimina</button>
+                            </form>
+                        </li>
                     @endforeach
                 </ul>
                 <a class="btn btn-primary" href="{{route('admin.categories.create')}}">Add a new category</a>
