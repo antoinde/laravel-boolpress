b13bf2945c8132220c10fb99ac98dc509c4ba1b8
antoinde <antonello.indelicato98@gmail.com>
2022-11-20 17:28:38 +0100

    launched TagSeeder

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Tag;
+use Illuminate\Support\Str;
 
 class TagSeeder extends Seeder
 {
@@ -12,5 +14,43 @@ class TagSeeder extends Seeder
     public function run()
     {
         //
+        // In tags table migration we have:
+        // $table->string('name', 30);
+        // $table->string('slug')->unique();
+
+        $tags = [
+            'Today',
+            'Yesterday',
+            'Tomorrow',
+            'Monday', 
+            'Thuesday', 
+            'Wednesday', 
+            'Thursday', 
+            'Friday', 
+            'Saturday', 
+            'Sunday',  
+            'Rock',   
+            'Metal',
+            'Blues',  
+            'House', 
+            'Chill', 
+            'R&B', 
+            'Pop', 
+            'Jazz', 
+            'Punk',
+            'Palermo',
+            'Mazara', 
+            'Castelvetrano',
+            'Marsala',    
+            'Messina',
+            'Campobello di Mazara',  
+        ];
+
+        foreach($tags as $element){
+            $tag = new Tag();
+            $tag->name = $element;
+            $tag->slug = Str::slug($tag->name);
+            $tag->save();
+        }
     }
 }
